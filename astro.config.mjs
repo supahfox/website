b4d0d1de72bca0a0ecd91da9b75c07de589a26e4
Fabri <31490939+SupahFox@users.noreply.github.com>
2023-09-28 03:55:34 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap';
+import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: 'https://astronaut.github.io',
-  base: '/website',
-})
+    site: 'https://astronaut.github.io',
+    base: '/website',
+    integrations: [sitemap(), svelte(), mdx()],
+    output: "server",
+    vite: {
+        build: {
+            sourcemap: true
+        }
+    },
+    markdown: {
+        syntaxHighlight: "prism"
+    },
+});
