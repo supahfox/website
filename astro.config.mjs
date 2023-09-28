c470112c6a5be2e338c982c1237a9346c6cfe47d
Fabri <31490939+SupahFox@users.noreply.github.com>
2023-09-28 03:50:23 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,6 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import svelte from "@astrojs/svelte";
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config'
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
-    site: "https://supahfox.live",
-    base: "/",
-    integrations: [sitemap(), svelte(), mdx()],
-    output: "server",
-    vite: {
-        build: {
-            sourcemap: true
-        }
-    },
-    markdown: {
-        syntaxHighlight: "prism"
-    },
-    adapter: cloudflare()
-});
+  site: 'https://astronaut.github.io',
+  base: '/website',
+})
