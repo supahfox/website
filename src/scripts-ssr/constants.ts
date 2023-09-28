98421a799f2b07ef003142869c4d81169f558a32
Fabri <31490939+SupahFox@users.noreply.github.com>
2023-09-28 02:49:32 -0300

    Update constants.ts

--- src/scripts-ssr/constants.ts
+++ src/scripts-ssr/constants.ts
@@ -1,5 +1,5 @@
-export const AUTHOR = "Vendicated";
-export const AUTHOR_URL = "https://github.com/Vendicated";
-export const ICON = "https://github.com/Vendicated.png?size=256";
+export const AUTHOR = "SupahFox";
+export const AUTHOR_URL = "https://github.com/SupahFox";
+export const ICON = "https://github.com/SupahFox.png?size=256";
 export const THEME_COLOUR = "#dd7878";
 export const SOURCE_CODE_URL = "https://codeberg.org/Ven/vendicated.dev";
