6f478d445ee96cfbd82c069f1fdcd20fc4bcd2e4
fab <31490939+supahfox@users.noreply.github.com>
2024-04-30 04:01:21 -0300

    Update constants.ts

--- src/scripts-ssr/constants.ts
+++ src/scripts-ssr/constants.ts
@@ -2,4 +2,4 @@ export const AUTHOR = "SupahFox";
 export const AUTHOR_URL = "https://github.com/SupahFox";
 export const ICON = "https://github.com/SupahFox.png?size=256";
 export const THEME_COLOUR = "#dd7878";
-export const SOURCE_CODE_URL = "https://github.com/SupahFox/supahfox.live";
+export const SOURCE_CODE_URL = "https://github.com/SupahFox/website";
